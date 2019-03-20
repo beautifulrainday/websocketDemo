51b976d948cb5c73fde6b0c95d2a5f7da8099bdd
beautifulrainday <854018001@qq.com>
2019-03-20 15:39:46 +0800

    加上拦截器，完整版，，双向交互

--- src/main/java/com/websocket/demo/config/WebSocketConfig.java
+++ src/main/java/com/websocket/demo/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.websocket.demo.config;
 
 import com.websocket.demo.handler.MyHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -12,11 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myHandler(), "myHandler/{ID}").setAllowedOrigins("*");
+        registry.addHandler(new MyHandler(), "myHandler/{ID}").setAllowedOrigins("*");
     }
 
-    public WebSocketHandler myHandler() {
-        return new MyHandler();
-    }
 
 }
